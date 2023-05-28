3fb60c9b78ce065264488feaa13b56cb651a833c
Dwiferdio Seagal Putra <dwiferdio.putra@alodokter.com>
2023-05-28 13:21:32 +0700

    set album reference in photo model to optional

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,3 @@
-# TODO: change into album
 class Album < ApplicationRecord
   has_many :photos
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,3 @@
 class Photo < ApplicationRecord
-  belongs_to :album
+  belongs_to :album, optional: true
 end
