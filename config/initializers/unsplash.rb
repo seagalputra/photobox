ab02e85d1dca08cf0e0f519d70aed729ca7dee09
seagalputra <seagalputra@gmail.com>
2023-12-21 12:04:37 +0700

    add a secret key for the production environment and add a condition for checking secret key when initializing unsplash api

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Unsplash.configure do |config|
-  config.application_access_key = Rails.application.credentials.unsplash.access_key!
-  config.application_secret = Rails.application.credentials.unsplash.secret_key!
-  config.application_redirect_uri = 'http://127.0.0.1:3000/oauth/callback'
-  config.utm_source = 'photobox_app'
+if Rails.application.credentials[:unsplash]
+  Unsplash.configure do |config|
+    config.application_access_key = Rails.application.credentials.unsplash.access_key!
+    config.application_secret = Rails.application.credentials.unsplash.secret_key!
+    config.application_redirect_uri = 'http://127.0.0.1:3000/oauth/callback'
+    config.utm_source = 'photobox_app'
+  end
 end
